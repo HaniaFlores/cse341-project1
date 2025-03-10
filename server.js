d019deb0eebf36cae3b8449d898531e0b94741d7
Hania Flores <hanniaflores03@gmail.com>
2025-03-09 22:27:20 -0600

    Updating GET Endpoint

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongodb = require('./data/database.js');
 const app = express();
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
@@ -14,5 +14,3 @@ mongodb.initDb((err) => {
         app.listen(port, () => {console.log(`Database is listening and node Running on port ${port}`)});
     }
 });
-
-app.listen(port, () => {console.log(`Runnnig on port ${port}`)});
